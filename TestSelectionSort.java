a49b1e90194decaa2c172cbe92c2f057265daf15
Akash Patel <akashvp@umich.edu>
2021-04-15 14:45:54 -0400

    Undo of Accidental Delete
    Added back code inside testing that was accidentally deleted.

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -53,9 +53,19 @@ public class TestSelectionSort {
 	}
 
 	public void testMixed() {
+		SelectionSort selectionSort = new SelectionSort();
+		int[] arr = {-2, -1, 1, 0};
+		int[] Sortedarr = {-2, -1, 0, 1};
+		selectionSort.basicSelectionSort(arr);
+		Assert.assertArrayEquals(arr, Sortedarr);
 	}
 
 	public void testDuplicated() {
+				SelectionSort selectionSort = new SelectionSort();
+		int[] arr = {0, 0, 1};
+		int[] Sortedarr = {0, 0, 1};
+		selectionSort.basicSelectionSort(arr);
+		Assert.assertArrayEquals(arr, Sortedarr);
 	}
 
 }
